0f607fd6ed6dbd2f5cad1a5a79b7b72ef5beba03
Eric Wallace <eric@ecrosstexas.com>
2022-01-21 00:11:54 -0600

    eleventy-plugin-directory-output
    add eleventy-plugin-directory-output

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 const passthroughs = require('./src/config/passthroughs');
 const collections = require('./src/config/collections');
+const directoryOutputPlugin = require("@11ty/eleventy-plugin-directory-output");
 const filters = require('./src/config/filters');
 const watchtargets = require('./src/config/watchtargets');
 const plugins = require('./src/config/plugins');
@@ -83,6 +84,20 @@ module.exports = function (eleventyConfig) {
   console.log("\n");
   console.groupEnd();
 
+/**
+   * https://www.11ty.dev/docs/plugins/directory-output/
+   */
+ eleventyConfig.setQuietMode(true);
+ eleventyConfig.addPlugin(directoryOutputPlugin, {
+   // Customize columns
+   columns: {
+     filesize: true, // Use `false` to disable
+     benchmark: true, // Use `false` to disable
+   },
+
+   // Will show in yellow if greater than this number of bytes
+   warningFileSize: 400 * 1000,
+ });
 
   /**
    * Configure browsersync
